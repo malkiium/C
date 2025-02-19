80eff54f74d7126ecdc85d2a21887be3b10fb23b
malkiium <elihalldelafosse@gmail.com>
2025-02-19 20:19:32 +0100

    Add trilcount.c to measure execution time of a loop

---
+++ trilcount.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+    long long int count = 0;
+
+    start = clock();
+    for (count = 0; count < 1000000000; count++) {
+        printf("%lld\n", count);
+    }
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
