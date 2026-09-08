2bf90ae96a02a64f92882745f0f7723eb04e74a1
malkiium <elihalldelafosse@gmail.com>
2026-09-08 14:33:52 +0200

    fix: correct format specifier for pointer value in 01.c

--- 2627/exos/01.c
+++ 2627/exos/01.c
@@ -24,7 +24,7 @@ int main() {
     int i = 35;
     printf("valeur de i = %d\n", i);
     void * ptr = &i;
-    printf("valeur de ptr = %d\n", ptr);
+    printf("valeur de ptr = %p\n", ptr);
     float a = *(float *)ptr;
     printf("valeur de a = %f\n", a);
     
