08d5ddf3f6426adf671c614a72f563725cf0d797
malkiium <elihalldelafosse@gmail.com>
2026-09-08 08:37:47 +0200

    feat: add size information for int, long, and char in 01.c

--- 2627/exos/01.c
+++ 2627/exos/01.c
@@ -2,4 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");
+    printf("taille int = %lu\n", sizeof(int));
+    printf("taille long = %lu\n", sizeof(long));
+    printf("taille char = %lu\n", sizeof(char));
 }
